c2df3afb157632a144ee1d275a4dea4c20e4857a
timoguy <msiskazikani@gmail.com>
2025-04-09 11:47:02 +0200

    added scafold and worked on receiving messages & controllers

--- lib/inbox.dart
+++ lib/inbox.dart
@@ -5,14 +5,14 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:intl/intl.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Chat(
-        roomName:'warge',
-        sender:"philimon"
+      roomName: 'warge',
+      sender: "philimon",
     ),
   ));
 }
@@ -20,7 +20,7 @@ void main() async{
 class Chat extends StatefulWidget {
   final String roomName;
   final String sender;
-  Chat({required this.roomName, required this.sender});
+  const Chat({required this.roomName, required this.sender});
 
   @override
   State<Chat> createState() => _ChatState();
@@ -28,170 +28,212 @@ class Chat extends StatefulWidget {
 
 class _ChatState extends State<Chat> {
   final TextEditingController _msg = TextEditingController();
-  final ScrollController _scroll=ScrollController();
+  final ScrollController _scroll = ScrollController();
   List<Widget> WidgetList2 = [];
 
+  @override
+  void initState() {
+    super.initState();
+    recieveMSG(); // 👈 Start receiving messages
+  }
 
-  void sendMSG(String msg) async{
+  void sendMSG(String msg) async {
     try {
-      DateTime now=DateTime.now();
+      DateTime now = DateTime.now();
       final DatabaseReference sender = FirebaseDatabase.instance.ref();
       sender.child(widget.roomName).push().set({
-        "msg":msg,
-        "sender":widget.sender,
-        "time":DateFormat('hh:mm a').format(now)
+        "msg": msg,
+        "sender": widget.sender,
+        "time": DateFormat('hh:mm a').format(now)
       });
-      print("msg sent");
-      //Update(msg, "send");
-    }catch(e){
+      _msg.clear();
+    } catch (e) {
       print("Failed $e");
     }
   }
 
-   void recieveMSG() async{
-     try{
-       final DatabaseReference reciever=FirebaseDatabase.instance.ref();
- 
-       reciever.child(widget.roomName).onChildAdded.listen((event){
-         final  key=event.snapshot.key.toString();
-         var data = event.snapshot.value;
-         if (data != null && data is Map && data.containsKey("msg")) {
-           if(data["sender"]!=widget.sender && data["sender"]!="start") {
-             Update(data["msg"],data["time"]!=null?data["time"]:"", "receive",data["sender"],key);
-             scrollToBottom();
-           }else{
-             if(data["sender"]!="start") {
-               Update(data["msg"],data["time"]!=null?data["time"]:"","send",data["sender"],key);
-               scrollToBottom();
-             }
-           }
-         } else {
-           print("Halla");
-         }
-       });
-     }catch(e){
-       print("Error: $e");
-     }
-   }
- 
-   void Delete(child1,key1)async{
-     print(key1);
-     final DatabaseReference deleter=FirebaseDatabase.instance.ref();
-     deleter.child("$child1/$key1").remove().then((_){
-       setState(() {
-         WidgetList2=[];
-       });
-       recieveMSG();
-     });
-   }
- 
-   void Update(message,time,type,sender,key) {
-     setState(() {
-       scrollToBottom();
-       WidgetList2.add(
-         GestureDetector(
-           onLongPress: (){
-             showAlertDialog2(context, "This message is going to be deleted", key);
-           },
-         child:Container(
-           width: double.infinity,
-           margin: EdgeInsets.symmetric(horizontal: 5),
-           child: Column(
-             crossAxisAlignment: type == "receive"
-                 ? CrossAxisAlignment.start
-                 : CrossAxisAlignment.end,
-             children: [
-               Container(
-                 constraints: BoxConstraints(minWidth:100,maxWidth: 250),
-                 margin: EdgeInsets.all(0),
-                 padding: EdgeInsets.all(5),
-                 child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                     Text(
-                       sender==widget.sender?"You":sender,
-                       style: GoogleFonts.roboto(color: Color(0xff41a7cd),fontSize: 10),
-                     ),
-                     Text(
-                       message,
-                       softWrap: true,
-                       style: GoogleFonts.roboto(
-                         color: Color(0xffddded9),
-                       ),
-                     ),
-                     Row(
-                       mainAxisSize: MainAxisSize.min,
-                       mainAxisAlignment: MainAxisAlignment.end,
-                       children: [
-                         Text(
-                           time!=""?time:"time unavailable",
-                           style: GoogleFonts.roboto(color: Color(0xff41a7cd),fontSize: 10),
-                         )
-                       ],
-                     ),
-                   ],
-                 ),
-                 decoration: BoxDecoration(
-                   color: Color(0xff0f212d),
-                   border: Border.all(color: Color(0xff0d2230), width: 1),
-                   borderRadius: BorderRadius.circular(10),
-                 ),
-               ),
-               SizedBox(height: 10),
-             ],
-           ),
-         ),
-         ),
-       );
-     });
-     scrollToBottom();
-   }
- 
- 
-   void showAlertDialog(BuildContext context, msg) {
-     showDialog(
-       context: context,
-       builder: (BuildContext context) {
-         return AlertDialog(
-           title: Text("Notice"),
-           content: Text(msg),
-           actions: [
-             TextButton(
-               child: Text("OK"),
-               onPressed: () {
-                 Navigator.of(context).pop();
-               },
-             ),
-           ],
-         );
-       },
-     );
-   }
+  void recieveMSG() async {
+    try {
+      final DatabaseReference receiver = FirebaseDatabase.instance.ref();
 
-   void showAlertDialog2(BuildContext context, msg,key) {
-     showDialog(
-       context: context,
-       builder: (BuildContext context) {
-         return AlertDialog(
-           title: Text("Notice"),
-           content: Text(msg),
-           actions: [
-             TextButton(
-               child: Text("Cancel"),
-               onPressed: () {
-                 Navigator.of(context).pop();
-               },
-             ),
-             TextButton(
-               child: Text("OK"),
-               onPressed: () {
-                 Delete(widget.roomName, key);
-                 Navigator.of(context).pop();
-               },
-             ),
-           ],
-         );
-       },
-     );
-   }
- 
+      receiver.child(widget.roomName).onChildAdded.listen((event) {
+        final key = event.snapshot.key.toString();
+        var data = event.snapshot.value;
+
+        if (data != null && data is Map && data.containsKey("msg")) {
+          String time = data["time"] ?? "";
+          if (data["sender"] != widget.sender && data["sender"] != "start") {
+            Update(data["msg"], time, "receive", data["sender"], key);
+          } else {
+            if (data["sender"] != "start") {
+              Update(data["msg"], time, "send", data["sender"], key);
+            }
+          }
+        }
+      });
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  void Delete(String child1, String key1) async {
+    final DatabaseReference deleter = FirebaseDatabase.instance.ref();
+    await deleter.child("$child1/$key1").remove();
+    setState(() {
+      WidgetList2 = [];
+    });
+    recieveMSG();
+  }
+
+  void Update(String message, String time, String type, String sender, String key) {
+    setState(() {
+      WidgetList2.add(
+        GestureDetector(
+          onLongPress: () {
+            showAlertDialog2(context, "This message will be deleted.", key);
+          },
+          child: Container(
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            child: Column(
+              crossAxisAlignment: type == "receive"
+                  ? CrossAxisAlignment.start
+                  : CrossAxisAlignment.end,
+              children: [
+                Container(
+                  constraints: BoxConstraints(minWidth: 100, maxWidth: 250),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Color(0xff0f212d),
+                    border: Border.all(color: Color(0xff0d2230), width: 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        sender == widget.sender ? "You" : sender,
+                        style: GoogleFonts.roboto(
+                            color: Color(0xff41a7cd), fontSize: 10),
+                      ),
+                      Text(
+                        message,
+                        softWrap: true,
+                        style: GoogleFonts.roboto(color: Color(0xffddded9)),
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            time != "" ? time : "time unavailable",
+                            style: GoogleFonts.roboto(
+                                color: Color(0xff41a7cd), fontSize: 10),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+
+    scrollToBottom();
+  }
+
+  void scrollToBottom() {
+    Future.delayed(Duration(milliseconds: 100), () {
+      if (_scroll.hasClients) {
+        _scroll.jumpTo(_scroll.position.maxScrollExtent);
+      }
+    });
+  }
+
+  void showAlertDialog2(BuildContext context, String msg, String key) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Delete Message"),
+          content: Text(msg),
+          actions: [
+            TextButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Delete"),
+              onPressed: () {
+                Delete(widget.roomName, key);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _msg.dispose();
+    _scroll.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff081c24),
+      appBar: AppBar(
+        backgroundColor: Color(0xff0f212d),
+        title: Text(widget.roomName.toUpperCase()),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              controller: _scroll,
+              padding: EdgeInsets.all(10),
+              children: WidgetList2,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+            color: Color(0xff0f212d),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _msg,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: "Type message",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    if (_msg.text.trim().isNotEmpty) {
+                      sendMSG(_msg.text.trim());
+                    }
+                  },
+                  icon: Icon(Icons.send, color: Color(0xff41a7cd)),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
