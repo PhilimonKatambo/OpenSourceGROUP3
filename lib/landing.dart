5da25692dddf0366cd5fd4e5f07481e7fdf961ce
priscilla_dyson <priscilladyson432@gmail.com>
2025-04-10 20:52:38 +0200

    updated landing file

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'inbox.dart';
@@ -9,7 +9,7 @@ void main() async {
   await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Colls(name: "timoguy@gmail.com"),
+    home: Colls(name: "priscilladyson@gmail.com"),
   ));
 }
 
@@ -23,20 +23,20 @@ class Colls extends StatefulWidget {
 
 class _CollsState extends State<Colls> {
   List<Widget> coll = [];
-  String user2 = "loading....";
+  late String user2 = "loading....";
 
   @override
   void initState() {
     super.initState();
     Online1();
+
   }
 
   void Online1() async {
     print('Hello');
     try {
-      final DatabaseReference receiver = FirebaseDatabase.instance.ref();
-
-      receiver.onChildAdded.listen((event) async {
+      final DatabaseReference reciever = FirebaseDatabase.instance.ref();
+      reciever.onChildAdded.listen((event) async {
         var roomName = event.snapshot.key.toString().replaceAll("/", "_");
         Update(roomName);
       });
@@ -45,13 +45,43 @@ class _CollsState extends State<Colls> {
     }
   }
 
+  void Delete(child1)async{
+    final DatabaseReference deleter=FirebaseDatabase.instance.ref();
+    deleter.child(child1).remove().then((_){
+      setState(() {
+        coll=[];
+      });
+      Online1();
+    });
+  }
+
+  void showAlertDialog(BuildContext context, msg) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Notice"),
+          content: Text(msg),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void Update(String roomName) async {
     setState(() {
       coll.add(
         GestureDetector(
           onTap: () {
             Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return Chat(roomName: roomName, sender: widget.name);
+              return Chat(roomName: roomName,sender: widget.name);
             }));
           },
           child: Container(
@@ -78,15 +108,10 @@ class _CollsState extends State<Colls> {
                 ]),
                 IconButton(
                   onPressed: () {
-                    widget.name.toLowerCase() == roomName.toLowerCase()
-                        ? Delete(roomName)
-                        : showAlertDialog(
-                        context, "Can't delete others inboxes");
+                    widget.name.toLowerCase()==roomName.toLowerCase()?Delete(roomName):showAlertDialog(context, "Can't delete others inboxes");
                   },
                   icon: Icon(
-                    widget.name.toLowerCase() == roomName.toLowerCase()
-                        ? Icons.delete
-                        : Icons.delete_forever_outlined,
+                    widget.name.toLowerCase()==roomName.toLowerCase()?Icons.delete:Icons.delete_forever_outlined,
                     color: Color(0xff2d87af),
                     size: 25,
                   ),
@@ -105,56 +130,45 @@ class _CollsState extends State<Colls> {
     });
   }
 
-  void Delete(String child1) async {
-    final DatabaseReference deleter = FirebaseDatabase.instance.ref();
-    deleter.child(child1).remove().then((_) {
-      setState(() {
-        coll = [];
-      });
-      Online1();
-    });
-  }
-
-  void showAlertDialog(BuildContext context, msg) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Notice"),
-          content: Text(msg),
-          actions: [
-            TextButton(
-              child: Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff0b1c26),
       appBar: AppBar(
-        title: Text("Welcome"),
-      ),
-      body: ListView(
-        children: coll.isNotEmpty
-            ? coll
-            : [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                user2,
-                style: GoogleFonts.poppins(fontSize: 20),
+          backgroundColor: const Color(0xff0b1c26),
+          leadingWidth: 0,
+          title:
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              "Online",
+              style: GoogleFonts.roboto(
+                textStyle: const TextStyle(
+                  color: Color(0xff0fd4fe),
+                  fontSize: 30,
+                ),
               ),
             ),
-          )
-        ],
+            Text(
+              widget.name==''?"Loading...":widget.name.toUpperCase(),
+              style: GoogleFonts.roboto(
+                textStyle: const TextStyle(
+                  color: Color(0xff0fd4fe),
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ])),
+      body: SingleChildScrollView(
+        child: coll.isEmpty?Center(
+            child:Container(
+                margin: EdgeInsets.only(top: 200),
+                width:50,height:50,
+                child:CircularProgressIndicator())):Container(
+          width: double.infinity,
+          child: Column(
+            children: [...coll],
+          ),
+        ),
       ),
     );
   }
